ab56bc622492a3ff949828f22d533f3ad1c467f4
joao <ifjoaovictorgomes@gmail.com>
2024-07-28 22:52:43 -0300

    atualização de site, consumo de api

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'loja'`, () => {
+  it(`should have as title 'GamesHub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('loja');
+    expect(app.title).toEqual('GamesHub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('loja app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('GamesHub app is running!');
   });
 });
